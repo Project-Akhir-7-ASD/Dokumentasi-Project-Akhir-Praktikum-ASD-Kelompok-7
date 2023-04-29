969aa6e8579028fcf9aa88d352d86b5bc31ea3c3
BAYU PURNAMA AJI <122213134+bayuprnmji31@users.noreply.github.com>
2023-04-29 22:25:25 +0800

    Delete data_staff.py

--- data_staff.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-cluster = MongoClient("mongodb+srv://kayla11virrly:yHe6dfatbJleGTfZ@cluster0.4a1rn0i.mongodb.net/test")
-database = cluster["DBMHS7"]
-collection = database["Data"]
